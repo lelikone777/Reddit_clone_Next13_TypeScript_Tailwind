c5438ee20b0988532089330532870a1bfd2ef945
lelikone777 <2257855@gmail.com>
2023-06-23 21:13:31 +0300

    subreddits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
 import { Toaster } from "@/components/ui/Toaster";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 // import {Inter} from "next/dist/compiled/@next/font/dist/google";
 import { Inter } from "next/font/google";
+import React from "react";
 
 export const metadata = {
   title: "Breadit",
@@ -12,14 +14,20 @@ export const metadata = {
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children, authModal }: { children: React.ReactNode; authModal: React.ReactNode }) {
   return (
     <html lang="en" className={cn("bg-white text-slate-900 antialiased light", inter.className)}>
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        <Navbar />
-        <div className="container max-w-7xl mx-auto h-full pt-12">{children}</div>
+        <Providers>
+          {/*{ @ts-expect-error server component }*/}
+          <Navbar />
 
-        <Toaster />
+          {authModal}
+
+          <div className="container max-w-7xl mx-auto h-full pt-12">{children}</div>
+
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,6 +1,16 @@
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/DropdownMenu";
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/DropdownMenu";
 import UserAvatar from "@/components/UserAvatar";
 import { User } from "next-auth";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
 import { FC } from "react";
 
 interface UserAccountNavProps {
@@ -19,6 +29,43 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
           }}
         />
       </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="bg-white" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && <p className="w-[200px] truncate text-sm text-zinc-700">{user.email}</p>}
+          </div>
+        </div>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href="/">Feed</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/r/create">Create community</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/settings">Settings</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem
+          onSelect={(event) => {
+            event.preventDefault();
+            signOut({
+              callbackUrl: `${window.location.origin}/sign-in`,
+            });
+          }}
+          className="cursor-pointer"
+        >
+          Sign Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
